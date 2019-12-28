2cf30a8d66eacf60c7825512d4559a8316de3d56
J. A. Hernández <joseanher81@gmail.com>
2019-12-28 13:35:50 +0100

    Some improvements in alerts service

--- src/app/services/alerts.service.ts
+++ src/app/services/alerts.service.ts
@@ -21,16 +21,11 @@ export class AlertsService {
 
   constructor(private airService: AirService, private sunService: SunService, private dataLocal: DataLocalService, private utilsService:UtilsService, private localNotifications: LocalNotifications, private backgroundGeolocation: BackgroundGeolocation) { }
 
-  /* async initAlerts() {
-    this.alerts = await this.dataLocal.loadAlerts();
-    this.startBackgroundGeolocation();
-  } */
-
   // Checks if an alert should be fired for the current location
   checkAlerts(maxDistance = 5, currentLat, currentLon, currentAirIndex, currentUvIndex) {
 
     this.alerts.forEach((alert) => {
-        // Check if the user is in the radiuos of an alert
+        // Check if the user is in the radius of an alert
        let distance = this.utilsService.ditanceFromTwoPoints(currentLat, currentLon, alert.lat, alert.lon);
         
        if(parseFloat(distance) < maxDistance) { // The user is inside the radius of an alert
@@ -61,13 +56,15 @@ export class AlertsService {
       desiredAccuracy: 10,
       stationaryRadius: 1,
       distanceFilter: 1,
-      interval: 600000,  // check every 10 minutes
-      debug: true, //  enable this hear sounds for background-geolocation life-cycle.
-      stopOnTerminate: false // enable this to clear background location settings when the app terminates
+      interval: 1200000,  // check every 20 minutes
+      debug: false, //  enable this hear sounds for background-geolocation life-cycle.
+      stopOnTerminate: false 
     };
 
+    // Load alerts from DDBB
     this.alerts = await this.dataLocal.loadAlerts();
     
+    // BackgroundGeolocation Cordova plugin configuration
     this.backgroundGeolocation.configure(config).then(() => {
       this.backgroundGeolocation
         .on(BackgroundGeolocationEvents.location)
@@ -80,7 +77,8 @@ export class AlertsService {
               this.sunService.getUvIndex(location.latitude, location.longitude).subscribe( resp => {
                 
                 let sunValue = resp.result.uv;
-                // Check alerts on DDBB
+                
+                // Check whether current data satisfies any alert on DDBB
                 this.checkAlerts(5, location.latitude, location.longitude, airValue, sunValue);
 
               });
